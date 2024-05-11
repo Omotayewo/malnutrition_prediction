3fcad2fd50aa5b492963fa5fac1d3e67a42b4cd1
Akinsanya Simeon Taiwo <akinsanyataiwo00@gmail.com>
2024-05-11 21:04:28 +0100

    Updated notebook

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Interface
+severe_wasting = st.number_input('Severe wasting (%)')
+wasting = st.number_input('Wasting (%)')
+stunting = st.number_input('Stunting (%)')
+underweight = st.number_input('Underweight (%)')
+overweight = st.number_input('Overweight (%)')
